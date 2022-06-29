d51a40bf8ca5d43707c8c53ee1af8d4b3dbcb899
nahid <comeback>
2022-06-29 18:05:56 +0600

    Template layout reorganized

---
+++ components/Template.js
@@ -0,0 +1,83 @@
+import { PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined, FileDoneOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import { useState } from 'react';
+import styles from '../styles/Template.module.less'
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileDoneOutlined />),
+];
+
+const Template = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh"
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div className={styles.logo} />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0
+          }}
+        />
+        <Content
+          style={{
+            margin: "0 16px"
+          }}
+        >
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360
+            }}
+          >
+            { children }
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center"
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Template;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Template from '../components/Template';
 import "../styles/antd.less";
 import '../styles/globals.less'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Template>
+      <Component {...pageProps} />
+    </Template>
+  )
 }
 
 export default MyApp
